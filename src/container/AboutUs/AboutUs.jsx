3e2e007b540d2e9913660502fcc47b886fbc5cfd
Nurain <nurainbamidelejnr@gmail.com>
2023-08-05 20:12:03 +0100

    first commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { images } from "../../constants";
+import "./AboutUs.css";
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Welcome to our restaurant, where passion for food meets exceptional
+          hospitality. With a rich culinary heritage and a dedication to
+          creating memorable dining experiences, we invite you to savor every
+          moment with us.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Founded with love in 2023, our restaurant's journey has been defined
+          by a commitment to culinary excellence, creating cherished memories
+          for guests, and evolving with the ever-changing tastes of our patrons.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
